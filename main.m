f1dd8a173215fbb38ffacb17c74c2eca0b85add1
VinsanityJr <42324584+VinsanityJr@users.noreply.github.com>
2022-10-11 13:01:20 -0400

    added binary image code
    added binary image code for crosshair detection

--- main.m
+++ main.m
@@ -1,3 +1,8 @@
+% make sure that no vid exists
+if exist('vid', 'var')
+    stop(vid)
+end
+
 % create a video object for matlab to get images from
 vid = videoinput('winvideo', 2, 'MJPG_640x480');
 
@@ -11,9 +16,24 @@ start(vid)
 % save the background image
 background_image = getdata(vid, 1);
 
+% parameters
+black_threshold = 0.2 * 255;
+
 % runtime variables
 frame_counter = 1;
 
+% get the grayscale of the background
+binary = zeros(480, 640);
+for m = 1:480
+    for n = 1:640
+        binary(m, n) = sum(background_image(m, n, :)) < black_threshold*3;
+    end
+end
+imshow(binary)
+
+% find the image crosshairs
+
+figure()
 % main loop
 while true
     % get the new image frame
@@ -22,6 +42,8 @@ while true
     
     % background subtraction
     delta = data(:,:,:,1) - background_image;
+    
+    % search for positions in the ring
     
     % show the image to the screen
     imshow(delta);
