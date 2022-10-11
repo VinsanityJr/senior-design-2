43f6a64d5cef5b124b1f74339edcafbfdf4276d4
VinsanityJr <42324584+VinsanityJr@users.noreply.github.com>
2022-10-11 19:29:01 -0400

    minor changes
    minor changes, cleaned up the data() matrix

--- main.m
+++ main.m
@@ -28,27 +28,46 @@ while true
     % get the new image frame
     frame_counter = frame_counter + 1;
     data = getdata(vid, frame_counter);
-    data = double(data) ./ double(max(max(data)));
+    data = squeeze(double(data) ./ double(max(max(data))));
     
     % background subtraction
     delta = data(:,:,:,1) - background_image;
     delta = abs(delta ./ max(max(delta)));
     
     % form a windowing matrix
-    threshold = 0.6;
+    threshold = 0.4;
     window = sum(delta, 3) > threshold * 3;
     window = medfilt2(window);
          
          
     % window the data
-    image(:, :, 1) = data(:, :, 1, 1) .* window;
-    image(:, :, 2) = data(:, :, 2, 1) .* window;
-    image(:, :, 3) = data(:, :, 3, 1) .* window;
+    image(:, :, 1) = data(:, :, 1) .* window;
+    image(:, :, 2) = data(:, :, 2) .* window;
+    image(:, :, 3) = data(:, :, 3) .* window;
     
     % search for positions in the ring
     [centers, radii] = imfindcircles(window, [20, 50]);
     
+    % get the pixel colors
+    if ~isempty(centers)
+        % make a vector to hold the color values
+        colors = zeros(length(centers(:, 1)), 3);
+        
+        % grab each pixel and the color values
+        for c = 1:length(centers(:, 1))
+            point = round(centers(c, :));
+            
+            % get the color for this center point. I tried doing this in one
+            % line; it broke spectacularly
+            colors(c, 1) = data(point(2), point(1), 1);
+            colors(c, 2) = data(point(2), point(1), 2);
+            colors(c, 3) = data(point(2), point(1), 3);
+        end
+        
+        colors
+    end
+    
     % show the image to the screen
     imshow(image, [0.0, 1.0]);
-    viscircles(centers, radii, 'EdgeColor', 'w');
+    viscircles(centers, radii, 'EdgeColor', 'm');
 end
